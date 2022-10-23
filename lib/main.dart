b8f62b1ea8a51bdbc02ea178dc1e06db9649cda1
Raudhatul Zannah <116431414+raudhatulzannah@users.noreply.github.com>
2022-10-23 22:33:37 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,303 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UI(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class UI extends StatefulWidget {
+  const UI({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<UI> createState() => _UIState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _UIState extends State<UI> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(
+          backgroundColor: Colors.orangeAccent,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_back_ios_new),
+            color: Colors.white,
+          ),
+          title: Text(
+            "Curse",
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert_sharp),
+              color: Colors.black,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  color: Colors.orangeAccent,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.elliptical(150, 150),
+                    bottomRight: Radius.elliptical(150, 150),
+                  )),
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 18),
+                              child: Text(
+                                'Spanish',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 40),
+                              ),
+                            ),
+                            Container(
+                              height: 30,
+                              width: 120,
+                              margin: EdgeInsets.all(30),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15)),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text('Begginer'),
+                                  IconButton(
+                                      icon: Icon(
+                                          Icons.keyboard_arrow_down_rounded),
+                                      onPressed: () {}),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Icon(Icons.diamond_outlined),
+                                  Icon(Icons.diamond_outlined),
+                                  Text('2 Milestones')
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Icon(
+                          Icons.circle_outlined,
+                          size: 100,
+                        ),
+                      ]),
+                ),
+                Expanded(
+                    child: GridView.count(
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    Card(
+                      margin: const EdgeInsets.all(40),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.orange,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(Icons.article),
+                            ),
+                          ),
+                          Text('Basics'),
+                          Text('4/5'),
+                          Row(
+                            children: [
+                              Container(
+                                height: 10,
+                                width: 80,
+                                margin: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    color: Colors.orange,
+                                    borderRadius: BorderRadius.circular(15)),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Card(
+                      margin: const EdgeInsets.all(40),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(Icons.shopping_bag),
+                            ),
+                          ),
+                          Text('Occupations'),
+                          Text('1/5'),
+                          Container(
+                            height: 10,
+                            width: 20,
+                            margin: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(15)),
+                          )
+                        ],
+                      ),
+                    ),
+                    Card(
+                      margin: const EdgeInsets.all(40),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 30, 172, 243),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(Icons.message),
+                            ),
+                          ),
+                          Text('Conversation'),
+                          Text('3/5'),
+                          Container(
+                            height: 10,
+                            width: 65,
+                            margin: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 30, 172, 243),
+                                borderRadius: BorderRadius.circular(15)),
+                          )
+                        ],
+                      ),
+                    ),
+                    Card(
+                      margin: const EdgeInsets.all(40),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(Icons.place),
+                            ),
+                          ),
+                          Text('Piaces'),
+                          Text('1/5'),
+                          Container(
+                            height: 10,
+                            width: 20,
+                            margin: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(15)),
+                          )
+                        ],
+                      ),
+                    ),
+                    Card(
+                      margin: const EdgeInsets.all(40),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 243, 7, 231),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(Icons.family_restroom),
+                            ),
+                          ),
+                          Text('Family members'),
+                          Text('3/5'),
+                          Container(
+                            height: 10,
+                            width: 65,
+                            margin: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 243, 7, 231),
+                                borderRadius: BorderRadius.circular(15)),
+                          )
+                        ],
+                      ),
+                    ),
+                    Card(
+                      margin: const EdgeInsets.all(40),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 94, 19, 224),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(Icons.apple),
+                            ),
+                          ),
+                          Text('Foods'),
+                          Text('2/5'),
+                          Container(
+                            height: 10,
+                            width: 40,
+                            margin: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 94, 19, 224),
+                                borderRadius: BorderRadius.circular(15)),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+              ],
+            )
+          ],
+        ));
   }
 }
